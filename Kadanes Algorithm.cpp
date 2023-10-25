d1e751e558da2160e8eb2d0ecf85517dafc80414
Shifatur-Rahman <59673387+Shifatur-Rahman@users.noreply.github.com>
2023-10-25 14:46:08 +0600

    Add files via upload

---
+++ Kadanes Algorithm.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int arr[] = {-2,1,-3,4,-1,2,1,-5,4};
+    int i, j, n;
+    n = sizeof(arr)/sizeof(arr[0]);
+    int local_max = 0;
+    int sum = 0;
+    for(i=0; i<n; i++)
+    {
+        sum = sum + arr[i];
+        local_max = max(sum,local_max);
+
+        if(sum<=0){
+          sum = 0;
+        }
+    }
+
+cout<<local_max<<endl;
+
+}
